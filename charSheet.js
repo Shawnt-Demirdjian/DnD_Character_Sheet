bbc679c677a512b2f9ee0c785b5c0a0bd25635ca
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2018-03-13 21:46:45 +0100

    source files added

---
+++ charSheet.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+
+});
+/*
+	$('#send').on("click", function () {
+		var url = "http://www.dnd5eapi.co/api/ides";
+
+		$.get(url, function (response) {
+			$('#phrase').html(response);
+			console.log(response);
+		});
+	});
+*/
