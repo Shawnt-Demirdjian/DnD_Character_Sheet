6616670f072e45fb80baf286488bd8c8e9495316
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2018-03-15 22:47:53 +0100

    weapon table row management functional

--- charSheet.js
+++ charSheet.js
@@ -1,33 +1,30 @@
 $(document).ready(function () {
+
+	/*Weapon Row Management*/
+
+	$("#add").click(function () {
+		$("#weaponRow").clone().prependTo("#weapons");
+		$("#rem").click(RemoveWeaponRow);
+
+	});
+	
+	$("#rem").click(RemoveWeaponRow);
+
+	function RemoveWeaponRow() {
+		if ($(".weaponRows").length <= 1) {
+			return;
+		}
+		$(this).parent().parent().remove();
+	}
+
+
+	/*SOMETHING ELSE*/
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 });
 
 
