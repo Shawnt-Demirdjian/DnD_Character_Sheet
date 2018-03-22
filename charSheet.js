e7840e1f67de722f183fd045f0a7ddc655f4647f
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2018-03-22 18:38:40 +0100

    dictionaries.js added; contains indexes for API calls. Weapon row detail fill completed

--- charSheet.js
+++ charSheet.js
@@ -5,10 +5,11 @@ $(document).ready(function () {
 	$("#add").click(function () {
 		$("#weaponRow").clone().prependTo("#weapons");
 		$("#rem").click(RemoveWeaponRow);
-		$(".modableWeapon").click(openWeaponModal);
+		$(".fillWeaponRow").click(fillWeaponRow);
 
 	});
 
+	$(".fillWeaponRow").click(fillWeaponRow);
 	$("#rem").click(RemoveWeaponRow);
 
 	function RemoveWeaponRow() {
@@ -17,6 +18,32 @@ $(document).ready(function () {
 		}
 		$(this).parent().parent().remove();
 	}
+
+	/*WEAPON ROW AUTO FILL*/
+
+	function fillWeaponRow() {
+		var currWeapon = $(this);
+		var title = $(this).parent().siblings("#weapName").children().val();
+		if (title in weaponsDict) {
+			var newUrl = url + "equipment/" + weaponsDict[title];
+			$.get(newUrl, function (response) {
+				currWeapon.parent().siblings("#damage").children().val(response.damage.dice_count + " d" + response.damage.dice_value);
+				currWeapon.parent().siblings("#damageType").children().val(response.damage.damage_type.name);
+				if (response.weapon_range == "Melee") {
+					currWeapon.parent().siblings("#range").children().val("Melee");
+				} else {
+					currWeapon.parent().siblings("#range").children().val(response.range.normal + "/" + response.range.long);
+				}
+				currWeapon.parent().siblings("#weight").children().val(response.weight + " pounds");
+				currWeapon.parent().siblings("#weapProp").children().val("");
+				currWeapon.parent().siblings("#weapProp").children().val(response.properties[0].name)
+				for (var i = 1; i < response.properties.length; i++) {
+					currWeapon.parent().siblings("#weapProp").children().val(currWeapon.parent().siblings("#weapProp").children().val() + ", " + response.properties[i].name)
+				}
+			});
+		}
+	}
+
 
 	/*LIST ITEM MANAGEMENT*/
 
@@ -64,7 +91,7 @@ $(document).ready(function () {
 	});
 
 	$(".autoMod").blur(function () { //updates all ability and skill mods
-		var natMod = Math.floor(($(this).val()-10)/2);
+		var natMod = Math.floor(($(this).val() - 10) / 2);
 		$(this).parent().next().children().val(natMod);
 		$.each($(this).parent().parent().parent().find(":not(:checked)"), function (index, skill) {
 			$(skill).parent().next().children().val(natMod);
@@ -93,7 +120,11 @@ $(document).ready(function () {
 
 	$(".modable").click(openModal);
 
-	$(".modableWeapon").click(openWeaponModal);
+	$(".modableClass").click(openClassModal);
+
+	$(".modableRace").click(openRaceModal);
+
+	$(".modableAbil").click(openAbilityModal);
 
 	$(".modableSkill").click(openSkillModal);
 
@@ -105,14 +136,42 @@ $(document).ready(function () {
 		$("#detailModal").modal('toggle');
 	}
 
-	function openWeaponModal() {
-		var title = $(this).parent().siblings("#weapName").children().val();
+	function openClassModal() {
+		var title = $(this).html();
+		$("#modTitle").html(title);
+		$("#detailModal").modal('toggle');
+	}
+
+	function openRaceModal() {
+		var title = $(this).next().val();
+		if (title in racesDict) {
+			var newUrl = url + "races/" + racesDict[title];
+			$.get(newUrl, function (response) {
+				$("#modBody").html("<b>Alingment: </b>" + response["alignment"] + "<br>" + "<b>Age: </b>" + response["age"] + "<br>" + "<b>Size: </b>" + response["size"] + "; " + response["size_description"]);
+			});
+		} else {
+			$("#modBody").html("Sorry, there is no race '" + title + "' in the D&D 5e SRD.");
+		}
+		$("#modTitle").html(title);
+		$("#detailModal").modal('toggle');
+	}
+
+	function openAbilityModal() {
+		var title = $(this).html();
+		var newUrl = url + "ability-scores/" + abilityDict[title];
+		$.get(newUrl, function (response) {
+			$("#modBody").html(response["desc"]);
+		});
 		$("#modTitle").html(title);
 		$("#detailModal").modal('toggle');
 	}
 
 	function openSkillModal() {
 		var title = $(this).html();
+		var newUrl = url + "skills/" + skillsDict[title];
+		$.get(newUrl, function (response) {
+			$("#modBody").html(response["desc"]);
+		});
 		$("#modTitle").html(title);
 		$("#detailModal").modal('toggle');
 	}
@@ -122,6 +181,8 @@ $(document).ready(function () {
 		$("#modTitle").html(title);
 		$("#detailModal").modal('toggle');
 	}
+
+
 
 
 }); //end document.ready funtion
